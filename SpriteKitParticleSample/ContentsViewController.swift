a1a4b1b0d51ce2da025497babf6ab0594ef3462c
akatsuki174 <dev2015js@gmail.com>
2016-05-15 13:02:34 +0900

    issue #2 基本的な遷移が完了

---
+++ SpriteKitParticleSample/ContentsViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ContentsViewController.swift
+//  SpriteKitParticleSample
+//
+//  Created by akatsuki174 on 2016/05/08.
+//  Copyright © 2016年 akatsuki174. All rights reserved.
+//
+
+import UIKit
+
+class ContentsViewController: UIViewController {
+    var text: String?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print(text)
+    }
+}
