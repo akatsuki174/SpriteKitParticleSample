6474f839824b4d6d1591745d72b3ae3427055f07
akatsuki174 <dev2015js@gmail.com>
2016-05-15 13:02:34 +0900

    基本的な遷移が完了

--- SpriteKitParticleSample/ViewController.swift
+++ SpriteKitParticleSample/ViewController.swift
@@ -8,19 +8,65 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    @IBOutlet weak var tableView: UITableView!
+    var selectedParticle:String?
+    var selectedRow: NSIndexPath?
+    let particles = [ParticleType.Bokeh, ParticleType.Fire, ParticleType.Fireflies, ParticleType.Magic, ParticleType.Rain, ParticleType.Smoke, ParticleType.Snow, ParticleType.Spark]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        self.view.backgroundColor = UIColor.blackColor()
+        tableView.delegate = self
+        tableView.dataSource = self
     }
-
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewDidDisappear(animated)
+        if (selectedRow != nil) {
+            tableView?.deselectRowAtIndexPath(selectedRow!, animated: false)
+        }
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
-
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return particles.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell: UITableViewCell = UITableViewCell(style: UITableViewCellStyle.Subtitle, reuseIdentifier: "Cell")
+        cell.textLabel?.text = particles[indexPath.row].rawValue
+        return cell
+    }
+    
+    func tableView(table: UITableView, didSelectRowAtIndexPath indexPath:NSIndexPath) {
+        selectedParticle = particles[indexPath.row].rawValue
+        performSegueWithIdentifier("showContentsVC",sender: nil)
+        selectedRow = indexPath
+    }
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject!) {
+        if (segue.identifier == "showContentsVC") {
+            let vc: ContentsViewController = (segue.destinationViewController as? ContentsViewController)!
+            vc.text = selectedParticle
+        }
+    }
+    
+    @IBAction func backFromContentsVC(segue:UIStoryboardSegue){
+        NSLog("FirstView")
+    }
 }
 
+enum ParticleType: String {
+    case Bokeh = "Bokeh"
+    case Fire = "Fire"
+    case Fireflies = "Fireflies"
+    case Magic = "Magic"
+    case Rain = "Rain"
+    case Smoke = "Smoke"
+    case Snow = "Snow"
+    case Spark = "Spark"
+}
